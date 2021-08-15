110a03953c1dd8f987187ff43dfff27314e57bda
Pimmsupa_ <88363819+ARTITAYA-pimsupaporn@users.noreply.github.com>
2021-08-15 10:08:47 +0700

    Update 1(2).cpp

--- again/1(2).cpp
+++ again/1(2).cpp
@@ -1,18 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<conio.h>
 
-int main()
-{
-	int num[2];
-	for (int i = 0; i < 2; i++)
-	{
-		scanf("%d", &num[i]);
-	}
-	printf("%d\n", num[0] + num[1]);
-	printf("%d\n", num[0] - num[1]);
-	printf("%d\n", num[0] * num[1]);
-	printf("%d\n", num[0] / num[1]);
+double positive(double num1, double num2) {
+	return num1 + num2;
+}
+double negative(double num1, double num2) {
+	return num1 - num2;
+}
+double multiple(double num1, double num2) {
+	return num1 * num2;
+}
+double quotient(double num1, double num2) {
+	return num1 / num2;
+}
 
+int main() {
+
+	double num1, num2;
+	scanf("%lf  %lf", &num1, &num2);
+	printf("positive = %.2lf\n", positive(num1, num2));
+	printf("negative = %.2lf\n", negative(num1, num2));
+	printf("multiple = %.2lf\n", multiple(num1, num2));
+	printf("quotient = %.2lf\n", quotient(num1, num2));
 	return 0;
 }
